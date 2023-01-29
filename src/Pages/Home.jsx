0ec8661523bab49d8d7ca7540db631ad32bb26cc
Muhammad Zidane Fajarianto <muhammadzidanefaj@gmail.com>
2023-01-29 12:26:53 +0700

    Perbaruan

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -55,13 +55,7 @@ function Home() {
     if (error) {
         return (
             <p>
-                {error.message}, if you get this error, the free API I used
-                might have stopped working, but I created a simple example that
-                demonstrate how this works,{" "}
-                <a href="https://codepen.io/Spruce_khalifa/pen/mdXEVKq">
-                    {" "}
-                    check it out{" "}
-                </a>{" "}
+            Api Mungkin bermasalah
             </p>
         );
     } else if (!isLoaded) {
